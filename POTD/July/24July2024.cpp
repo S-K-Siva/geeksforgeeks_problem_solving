4f060d86ad1f156a814629db3b7495f3368b45cc
S.K.Siva <88235149+S-K-Siva@users.noreply.github.com>
2024-07-24 22:28:05 +0530

    added optimal solution for today's potd

--- POTD/July/24July2024.cpp
+++ POTD/July/24July2024.cpp
@@ -34,3 +34,24 @@ class Solution {
         return vv1 == vv;
     }
 };
+// recursion way!
+
+// bool isBSTUtil(Node* node, int min, int max) {
+//     // An empty tree is a BST
+//     if (node == nullptr) {
+//         return true;
+//     }
+
+//     // False if this node violates the min/max constraint
+//     if (node->data < min || node->data > max) {
+//         return false;
+//     }
+
+//     // Otherwise check the subtrees recursively, tightening the min or max constraint
+//     return isBSTUtil(node->left, min, node->data - 1) && 
+//            isBSTUtil(node->right, node->data + 1, max);
+// }
+
+// bool isBST(Node* root) {
+//     return isBSTUtil(root, INT_MIN, INT_MAX);
+// }
