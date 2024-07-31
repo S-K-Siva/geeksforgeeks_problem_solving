af4115b1914bff67d46ce9ecd4136e2f5533ed2b
S.K.Siva <88235149+S-K-Siva@users.noreply.github.com>
2024-07-31 23:59:41 +0530

    solvec potd

---
+++ POTD/July/31July2024.cpp
@@ -0,0 +1,14 @@
+string longestCommonPrefix(vector<string> arr) {
+        // your code here
+        string temp = arr[0];
+        int i=0,j=0;
+        while(i<temp.length()){
+            for(j=0;j<arr.size();j++){
+                if(temp[i]!=arr[j][i])break;
+            }
+            if(j!=arr.size())break;
+            i++;
+        }
+        if(i==0)return "-1";
+        return temp.substr(0,i);
+    }
