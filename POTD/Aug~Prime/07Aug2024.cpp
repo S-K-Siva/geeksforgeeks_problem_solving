7267f05e1fdc40d14df666851b21f0b48465fb49
S.K.Siva <88235149+S-K-Siva@users.noreply.github.com>
2024-08-07 12:15:48 +0530

    solved potd

---
+++ POTD/Aug~Prime/07Aug2024.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    int kthElement(int k, vector<int>& arr1, vector<int>& arr2) {
+        arr1.insert(arr1.end(),arr2.begin(),arr2.end());
+        sort(arr1.begin(),arr1.end());
+        return arr1[k-1];
+    }
+};
