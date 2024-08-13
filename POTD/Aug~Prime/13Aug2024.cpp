00748af2de83f4b7da722c1509bbb45099fabb3c
S.K.Siva <88235149+S-K-Siva@users.noreply.github.com>
2024-08-13 18:17:29 +0530

    solved potd

---
+++ POTD/Aug~Prime/13Aug2024.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    long long int floorSqrt(long long int n) {
+        return (long long int) floor(sqrt(n));
+    }
+};
