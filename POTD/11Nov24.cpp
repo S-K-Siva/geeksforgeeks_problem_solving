dfcae48464f441a514320b5d22e5d50ffe739968
Siva S K <88235149+S-K-Siva@users.noreply.github.com>
2024-11-11 22:56:22 +0530

    solved potd

---
+++ POTD/11Nov24.cpp
@@ -0,0 +1,17 @@
+class Solution {
+  public:
+    int minIncrements(vector<int>& arr) {
+        sort(arr.begin(),arr.end());
+        int res = 0;
+        
+        for(int i = 1;i<arr.size();i++){
+            if(arr[i] <= arr[i-1]){
+                int inc = arr[i-1] - arr[i] + 1;
+                arr[i] += inc;
+                res += inc;
+            }
+        }
+        
+        return res;
+    }
+};
