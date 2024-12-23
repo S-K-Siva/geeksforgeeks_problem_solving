30b7f4329da5cf6d57fe49a46dd8af5f9f8cc7f5
Siva Sakthivel <88235149+S-K-Siva@users.noreply.github.com>
2024-12-23 23:09:37 +0530

    solved potd :)

---
+++ 23Dec24.cpp
@@ -0,0 +1,30 @@
+
+class Solution {
+  public:
+    // Function to search a given number in row-column sorted matrix.
+    bool searchRowMatrix(vector<vector<int>> &mat, int x) {
+        int n = mat.size();
+        
+        for(int i = 1;i<n;i++){
+            mat[0].insert(mat[0].end(),mat[i].begin(),mat[i].end());
+        }
+        
+        vector<int> arr = mat[0];
+        sort(arr.begin(),arr.end());
+        int high = arr.size() - 1;
+        int low = 0;
+        
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            
+            if(arr[mid] == x){
+                return true;
+            }else if(arr[mid] > x){
+                high = mid - 1;
+            }else{
+                low = mid + 1;
+            }
+        }
+        return false;
+    }
+};
